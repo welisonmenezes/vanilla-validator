22b71836538e4ed7beb750bac0f5174b60baf8fd
welisonmenezes <welisonmenezes@hotmail.com>
2018-12-01 13:43:13 -0200

    change structure of project

--- vanilla-validator.js
+++ vanilla-validator.js
@@ -1,537 +1,99 @@
-var VanillaValidator = (function(){
+var VVchecks = (function(){
 
-	var querySelector, forms;
+	function VVchecks(){};
 
-	var defaultConfig = {
-		selectors: {
-			required: "required",
-			email: "email",
-			phone: "phone",
-			cpf: "cpf",
-			cnpj: "cnpj",
-			error: "error",
-			formError: "form-error",
-			messageError: "msg-error"
-		},
-		messages: {
-			required: "Required Filed",
-			email: "Invalid Email"
-		},
-		novalidateHTML5: true,
-		clearErrosOnChange: true,
-		callbacks: {
-			CB_FieldEach: null,
-			CB_Required: null,
-			CB_Required: null,
-			CB_Email: null,
-			CB_EmailEach: null,
-			CB_BeforeValidate: null,
-			CB_AfterValidate: null,
-			CB_Error: null,
-			CB_ClearErrors: null,
-			CB_ClearErrorsEach: null,
-			CB_Success: null
-		},
-		overrides: {
-			OV_Email: null,
-			OV_Required: null
-		}
+	VVchecks.prototype.isEmpty = function(value){
+		return value === "" || value.length < 1;
 	};
 
-	var apiConfig = {};
-
-	/**
-	 * return all forms that query selector matches
-	 *
-	 * @method getForms
-	 * @return {NodeList || null}
-	 */
-	var getForms = function(){
-		var forms = document.querySelectorAll(querySelector);
-		if(forms){
-			return forms;
-		}
-		return null;
+	VVchecks.prototype.isEmail = function(value){
+		var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+		return pattern.test(value);
 	};
 
-	/**
-	 * turns the forms novalidate by defautl
-	 *
-	 * @method setHTML5NoValidate
-	 * @param {NodeList} list of forms
-	 */
-	var setHTML5NoValidate = function(forms){
-		
-		if(!apiConfig.novalidateHTML5) return;
+	VVchecks.prototype.isInteger = function(value){
+        return /^[0-9]+$/.test(value);
+    };
 
-		if(forms){
-			var i, total = forms.length;
-			for(i = 0; i < total; i++){
-				forms[i].setAttribute("novalidate", true);
-			}
-		}
+    VVchecks.prototype.isDigit = function(value){
+        return /^[a-zA-Z0-9]+$/.test(value);
+    };
+
+    VVchecks.prototype.isUrl = function(value){
+        var pattern = new RegExp(/^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i);
+        return pattern.test(value);
+    };
+
+    VVchecks.prototype.hasLessThan = function(value, size){
+        return (value.length < size) ? true : false;
+    };
+
+    VVchecks.prototype.hasMoreThan = function(value, size){
+        return (value.length > size) ? true : false;
+    };
+
+    VVchecks.prototype.hasRangeLength = function(value, min, max){
+        return ( (value.length <= max) && (value.length >= min) ) ? true : false;
+    };
+
+    VVchecks.prototype.hasEqualLength = function(value, size){
+        return ( value.length == size ) ? true : false;
+    };
+
+	VVchecks.prototype.isObject = function(object){
+		return (object != false && typeof object === 'object' && object instanceof Object && (typeof object !== 'function'));
 	};
 
-	/**
-	 * handle the event submit of form
-	 *
-	 * @method addEventToFormsAndFields
-	 */
-	var addEventToFormsAndFields = function(){
-		if(forms){
-			var i, total = forms.length;
-
-			for(i = 0; i < total; i++){
-
-				forms[i].addEventListener("submit", function(event){
-					event.preventDefault();
-
-					var errors = 0;
-
-					callCallbackFunction(apiConfig.callbacks.CB_BeforeValidate, this);
-
-					removeValidationViewOfAll(this);
-
-					if(callOverridesFunction(apiConfig.overrides.OV_Email, this)){
-						if(! callOverridesFunction(apiConfig.overrides.OV_Email, this)()){
-							callCallbackFunction(apiConfig.callbacks.CB_Email, this);
-							errors++;
-						}
-					}else{
-						if(! applyValidationEmail(this)){
-							callCallbackFunction(apiConfig.callbacks.CB_Email, this);
-							errors++;
-						}
-					}
-					
-					if(callOverridesFunction(apiConfig.overrides.OV_Required, this)){
-						if(! callOverridesFunction(apiConfig.overrides.OV_Required, this)()){
-							callCallbackFunction(apiConfig.callbacks.CB_Required, this);
-							errors++;
-						}
-					}else{
-						if(! applyValidationRequired(this)){
-							callCallbackFunction(apiConfig.callbacks.CB_Required, this);
-							errors++;
-						}
-					}
-
-
-					if(errors > 0){
-						this.classList.add(apiConfig.selectors.formError);
-						callCallbackFunction(apiConfig.callbacks.CB_Error, this);
-					}else{
-
-					}
-
-					callCallbackFunction(apiConfig.callbacks.CB_AfterValidate, this);
-
-					return false;
-					
-				});
-
-				// if clearErrosOnChange is configured as true
-				if(apiConfig.clearErrosOnChange){
-
-					var fields = getFields(forms[i], apiConfig.selectors.required);
-
-					if(fields){
-						var x,
-							total = fields.length;
-
-						for(x = 0; x < total; x++){
-							// clear onchange
-							fields[x].addEventListener("change", function(){
-								if(this.classList.contains(apiConfig.selectors.error)){
-									removeValidationView(this, forms[i]);
-								}
-							});
-
-							// clear on keyup
-							fields[x].addEventListener("keyup", function(){
-								if(this.classList.contains(apiConfig.selectors.error)){
-									removeValidationView(this, forms[i]);
-								}
-							});
-						}
-					}
-
-				}
-				
-			}
-		}
-	};
-
-	/**
-	 * get required fields from especified form
-	 *
-	 * @method getFields
-	 * @param {NodeList} list of forms
-	 * @param {string} class css as a selector
-	 * @return {NodeList || null} returns the fields or null
-	 */
-	var getFields = function(form, selector){
-		if(form){
-			var fields = form.querySelectorAll('.' + selector);
-			if(fields){
-				return fields;
-			}
-		}
-		return null;
-	};
-
-	/**
-	 * applies required validation rules on fields and
-	 * return if this rule is valid for all fields
-	 *
-	 * @method applyValidationRequired
-	 * @param {NodeList} list of forms
-	 * @return {Boolean} if is valid returns true, otherwise returns false
-	 */
-	var applyValidationRequired = function(form){
-		var ret = true;
-		if(form){
-
-			var fields = getFields(form, apiConfig.selectors.required);
-	
-			if(fields){ 
-
-				var i,
-					total = fields.length,
-					checkboxRadios = {};
-
-				for(i = 0; i < total; i++){
-
-					if(fields[i].type !== 'checkbox' && fields[i].type !== 'radio'){
-						// fildes text, email, tel, textarea, select, inputfile
-
-						if(fields[i].value === "" || fields[i].value.length < 1){
-							// is inválid
-
-							// callbacks
-							callCallbackFunction(apiConfig.callbacks.CB_FieldEach, form, fields[i]);
-							callCallbackFunction(apiConfig.callbacks.CB_RequiredEach, form, fields[i]);
-
-							addValidationView(fields[i], apiConfig.messages.required);
-							ret = false;
-
-						}
-
-					}else{
-						// checkbox and radio fields
-
-						// add checkbox and radio fields into array to validate after
-						if(! checkboxRadios[fields[i].name]){
-							checkboxRadios[fields[i].name] = [];
-
-							// item responsible for validating the field
-							checkboxRadios[fields[i].name].push("invalid");
-						}
-						checkboxRadios[fields[i].name].push(fields[i]);
-
-					}
-
-				}
-
-				// validate checkbox and radio fields
-				for(cr in checkboxRadios){
-					if(checkboxRadios[cr].length){
-
-						var x,
-							totalCR = checkboxRadios[cr].length;
-
-						for(x = 1; x < totalCR; x++){
-							if(checkboxRadios[cr][x].checked){
-								// turn fiedl valid if it is checked
-								checkboxRadios[cr][0] = "valid";
-							}
-						}
-
-						if(checkboxRadios[cr][0] === "invalid"){
-							// is invalid
-
-							// callbacks
-							callCallbackFunction(apiConfig.callbacks.CB_FieldEach, form, checkboxRadios[cr]);
-							callCallbackFunction(apiConfig.callbacks.CB_RequiredEach, form, checkboxRadios[cr]);
-							
-							addValidationView(checkboxRadios[cr], apiConfig.messages.required);
-							ret = false;
-						}
-					}
-				}
-
-				
-			}
-			
-		}
-		return ret;
-	};
-
-	/**
-	 * applies email validation rules on fields and 
-	 * return if this rule is valid for all fields
-	 *
-	 * @method applyValidationEmail
-	 * @param {NodeList} list of forms
-	 * @return {Boolean} if is valid returns true, otherwise returns false
-	 */
-	var applyValidationEmail = function(form){
-		var ret = true;
-		if(form){
-
-			var fields = getFields(form, apiConfig.selectors.email);
-	
-			if(fields){ 
-
-				var i,
-					total = fields.length;
-
-				var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-
-				for(i = 0; i < total; i++){
-
-					if(!pattern.test(fields[i].value)){
-						// is invalid
-
-						// callbacks
-						callCallbackFunction(apiConfig.callbacks.CB_FieldEach, form, fields[i]);
-						callCallbackFunction(apiConfig.callbacks.CB_EmailEach, form, fields[i]);
-                    	
-                    	addValidationView(fields[i], apiConfig.messages.email);
-						ret = false;
-
-					}
-				}
-
-			}
-
-		}
-		return ret;
-	};
-
-	/**
-	 * applies view feedback of validation
-	 *
-	 * @method addValidationView
-	 * @param {NodeList || Array} list of elements or array with checkbox and radio fileds 
-	 */
-	var addValidationView = function(element, message){
-		if(element){
-
-			var parentEl = (element.constructor.name === "Array") ? element[element.length-1].parentElement : element.parentElement;
-
-			if(parentEl){
-				var messageContainer = document.createElement("SPAN");
-				messageContainer.innerHTML = message;
-
-				var messageClass = document.createAttribute("class");
-				messageClass.value = apiConfig.selectors.messageError;
-				messageContainer.setAttributeNode(messageClass);
-
-				
-				if(element.constructor.name === "Array"){
-					var i, totalEl = element.length;
-
-					for(i = 1; i < totalEl; i++){
-						element[i].classList.add(apiConfig.selectors.error);
-					}
-				}else{
-					element.classList.add(apiConfig.selectors.error);
-				}
-				
-
-				var oldMessages = parentEl.querySelectorAll("." + apiConfig.selectors.messageError);
-
-				if(oldMessages){
-					var x, totalOld = oldMessages.length;
-					for(x = 0; x < totalOld; x++){
-						oldMessages[x].remove();
-					}
-				}
-
-				parentEl.append(messageContainer);
-			}
-			
-		}
-	};
-
-	/**
-	 * remove the feedback views of validation
-	 *
-	 * @method removeValidationView
-	 * @param {NodeList || Array} list of elements or array with checkbox and radio fileds 
-	 */
-	var removeValidationView = function(element, form){
-		if(element){
-
-			var parentEl = (element.constructor.name === "Array") ? element[element.length-1].parentElement : element.parentElement;
-
-			if(parentEl){
-
-				callCallbackFunction(apiConfig.callbacks.CB_ClearErrorsEach, form, element);
-
-				if(element.constructor.name === "Array"){
-					var i, totalEl = element.length;
-
-					for(i = 1; i < totalEl; i++){
-						element[i].classList.remove(apiConfig.selectors.error);
-					}
-				}else{
-					element.classList.remove(apiConfig.selectors.error);
-				}
-
-				var oldMessages = parentEl.querySelectorAll("." + apiConfig.selectors.messageError);
-
-				if(oldMessages){
-					var x, totalOld = oldMessages.length;
-					for(x = 0; x < totalOld; x++){
-						oldMessages[x].remove();
-					}
-				}
-
-			}
-		
-		}
-	};
-
-	/**
-	 * applies removeValidationView for each fiel with error class
-	 *
-	 * @method removeValidationViewOfAll
-	 * @param {NodeList} list of forms
-	 */
-	var removeValidationViewOfAll = function(form){
-		if(form){
-			var fields = getFields(form, apiConfig.selectors.error);
-
-			callCallbackFunction(apiConfig.callbacks.CB_ClearErrors, form);
-
-			form.classList.remove(apiConfig.selectors.formError);
-	
-			if(fields){ 
-
-				var i,
-					total = fields.length;
-
-				for(i = 0; i < total; i++){
-					removeValidationView(fields[i], form);
-				}
-			}
-		}
-	};
-
-	/**
-	 * check if is an object
-	 *
-	 * @method isObject
-	 * @param {Object} A configuration object
-	 * @return {Boolean}
-	 */
-	var isObject = function(object){
-		return (object && typeof object === 'object' && object instanceof Object && (typeof object !== 'function'));
-	};
-
-	/**
-	 * merget default configuration with user configuration objects in apiConfig object
-	 *
-	 * @method mergeObjectsDeeply
-	 * @param {Object} A default configuration object
-	 * @param {Object} A user configuration object
-	 * @param {Object} The apiConfig global object
-	 */
-	var mergeObjectsDeeply = function(objectDefault, objectUser, target){
-
-		if(isObject(objectDefault) && isObject(objectUser) && isObject(target)){
-
-			for(t in objectDefault){
-
-				if(isObject(objectDefault[t]) && isObject(objectDefault[t])){
-
-					target[t] = objectDefault[t];
-
-					// applying recursion to copy deeply
-					mergeObjectsDeeply(objectDefault[t], objectUser[t], target[t]);
-
-				}else{
-
-					if(objectUser[t] !== undefined){
-						target[t] = objectUser[t];
-					}else{
-						target[t] = objectDefault[t];
-					}
-
-				}
-
-			}
-
-		}
-	};
-
-	/**
-	 * check if callback is a function and applies object refence as its this
-	 *
-	 * @method callCallbackFunction
-	 * @param {Function} the callback function
-	 * @param {NodeList || Object} the Object reference, can be the form or a specific field
-	 */
-	var callCallbackFunction = function(callback, ref, element){
-		if(typeof callback === 'function'){
-			callback.call(ref, element, apiConfig);
-		}
-	};
-
-	var callOverridesFunction = function(fn, ref, element){
-		if(typeof fn === 'function'){
-			return fn.bind(ref, element, apiConfig);
-		}
-		return null;
-	};
-
-	/**
-	 * Form validator class with pure Javascript
-
-	 * @class VanillaValidator
-	 * @constructor
-	 * @param {String} query selector
-	 */
-	function VanillaValidator(query, config){
-		querySelector = query;
-		forms = getForms();
-
-		mergeObjectsDeeply(defaultConfig, config, apiConfig);
-
-		setHTML5NoValidate(forms);
-
-		addEventToFormsAndFields();
-	};
-
-	return VanillaValidator;
-
+	VVchecks.prototype.isDate = function(value){
+        var check = false,
+        re = /^\d{1,2}\/\d{1,2}\/\d{4}$/,
+        adata, gg, mm, aaaa, xdata;
+        if ( re.test(value)) 
+        {
+            adata = value.split("/");
+            gg = parseInt(adata[0], 10);
+            mm = parseInt(adata[1], 10);
+            aaaa = parseInt(adata[2], 10);
+            xdata = new Date(aaaa, mm - 1, gg, 12, 0, 0, 0);
+            if ( ( xdata.getUTCFullYear() === aaaa ) && ( xdata.getUTCMonth () === mm - 1 ) && ( xdata.getUTCDate() === gg ) ) 
+            {
+                check = true;
+            } 
+            else 
+            {
+                check = false;
+            }
+        } 
+        else 
+        {
+            check = false;
+        }
+        return check;
+    };
+
+
+	return VVchecks;
 }());
 
+var vv = new VVchecks();
 
-var configuracoes = {
-	novalidateHTML5: true,
-	clearErrosOnChange: true,
-	callbacks: {
-		///*
-		CB_Error: function(element, configurations){
-			console.log('CB', element);
-			console.log('CB', configurations);
-			//configurations.messages.required = "custom Messagem"
-			console.log('CB', this);
-		}
-		//*/
-	},
-	overrides: {
-		OV_Email: function(element, configurations){
-			console.log('OV', element);
-			console.log('OV', configurations);
-			//configurations.messages.required = "custom Messagem"
-			console.log('OV', this);
+//console.log(vv)
+console.log('Está vazio? ', vv.isEmpty(''));
+console.log('É um email? ', vv.isEmail('email@email.com'));
+console.log('É um objeto? ', vv.isObject({}));
+console.log('É um inteiro? ', vv.isInteger(10000));
+console.log('É um digito? ', vv.isDigit('a165'));
+console.log('É uma url? ', vv.isUrl('https://www.google.com'));
+console.log('Tem menos que 5? ', vv.hasLessThan('weli', 5));
+console.log('Tem mais que 5? ', vv.hasMoreThan('welison', 5));
+console.log('Está entre 5 e 7? ', vv.hasRangeLength('welison', 5, 7));
+console.log('Tem exatamente 7? ', vv.hasEqualLength('welison', 7));
+console.log('É uma data? ', vv.isDate('12/12/2006'));
 
-			return false;
-		}
-	}
-};
-var valid = new VanillaValidator("form", configuracoes);
+
+var VanillaValidator = (function(){
+
+	function VanillaValidator(query, config){};
+
+	return VanillaValidator;
+}());
