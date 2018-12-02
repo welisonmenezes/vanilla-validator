e7614a9c0eb9f30fbc82d6e06828e4b959749edd
welisonmenezes <welisonmenezes@hotmail.com>
2018-12-02 11:07:53 -0200

    add more validations rules

--- vanilla-validator.js
+++ vanilla-validator.js
@@ -23,20 +23,101 @@ var VVchecks = (function(){
     };
 
     VVchecks.prototype.hasLessThan = function(value, size){
-        return (value.length < size) ? true : false;
+        return (value.length < size);
     };
 
     VVchecks.prototype.hasMoreThan = function(value, size){
-        return (value.length > size) ? true : false;
+        return (value.length > size);
     };
 
     VVchecks.prototype.hasRangeLength = function(value, min, max){
-        return ( (value.length <= max) && (value.length >= min) ) ? true : false;
+        return ( (value.length <= max) && (value.length >= min) );
     };
 
     VVchecks.prototype.hasEqualLength = function(value, size){
-        return ( value.length == size ) ? true : false;
+        return ( value.length == size );
     };
+
+    VVchecks.prototype.isMin = function(value, min){
+        return (value < min);
+    };
+
+    VVchecks.prototype.isMax = function(value, max){
+        return (value > max);
+    };
+
+    VVchecks.prototype.isRange = function(value, min, max){
+        return ( (value <= max) && (value >= min) );
+    };
+
+    VVchecks.prototype.equalTo = function(value, param){
+        return value === param;
+    };
+
+    VVchecks.prototype.isCpf = function(value){
+	    var CPF = value, cpfv, x;  
+
+	    if(!CPF) return false;
+
+	    cpfv  = CPF;
+	    if(cpfv.length == 14 || cpfv.length == 11) {
+	        cpfv = cpfv.replace('.', '');
+	        cpfv = cpfv.replace('.', '');
+	        cpfv = cpfv.replace('-', '');
+
+	        var nonNumbers = /\D/;
+	        
+	        if(nonNumbers.test(cpfv)){
+	            return false;
+	        } else{
+	            if (cpfv == "00000000000" ||
+	                cpfv == "11111111111" ||
+	                cpfv == "22222222222" ||
+	                cpfv == "33333333333" ||
+	                cpfv == "44444444444" ||
+	                cpfv == "55555555555" ||
+	                cpfv == "66666666666" ||
+	                cpfv == "77777777777" ||
+	                cpfv == "88888888888" ||
+	                cpfv == "99999999999") return false;
+
+	            var a = [];
+	            var b = 0;
+	            var c = 11;
+	            for(var i=0; i<11; i++){
+	                a[i] = cpfv.charAt(i);
+	                if (i < 9) b += (a[i] * --c);
+	            }
+
+	            if((x = b % 11) < 2){
+	                a[9] = 0;
+	            } else{
+	                a[9] = 11-x;
+	            }
+
+	            b = 0;
+	            c = 11;
+	            for (var y=0; y<10; y++) b += (a[y] * c--);
+
+	            if((x = b % 11) < 2){
+	                a[10] = 0;
+	            } else{
+	                a[10] = 11-x;
+	            }
+
+	            if((cpfv.charAt(9) != a[9]) || (cpfv.charAt(10) != a[10])) return false;
+	        }
+	    } else{
+	        if(cpfv.length === 0){
+	            return false;
+	        
+	        } else{
+	            return false;
+	        }
+	    }
+
+	    return true;
+	};
 
 	VVchecks.prototype.isObject = function(object){
 		return (object != false && typeof object === 'object' && object instanceof Object && (typeof object !== 'function'));
@@ -144,6 +225,9 @@ console.log('Tem mais que 5? ', vv.hasMoreThan('welison', 5));
 console.log('Está entre 5 e 7? ', vv.hasRangeLength('welison', 5, 7));
 console.log('Tem exatamente 7? ', vv.hasEqualLength('welison', 7));
 console.log('É uma data? ', vv.isDate('12/12/2006'));
-
-
+console.log('É menor que 5? ', vv.isMin(4, 5));
+console.log('É maior que 5? ', vv.isMax(6, 5));
+console.log('Está entre 3 e 5? ', vv.isRange(4, 3, 5));
+console.log('É igual a 5? ', vv.equalTo(5, 5));
+console.log('É um cpf? ', vv.isCpf('061.052.500-02'));
 
